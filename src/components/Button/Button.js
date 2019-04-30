687359cf4c895380849c6e6a83cfe125045e768b
vonlaroche <dominic.monika@gmail.com>
2019-04-30 16:50:50 +0200

    destructured props for button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Button.css";
 
-const button = (props, {children}) => {
-    return <button onClick={props.clicked} className="btnStyle">{props.children}</button>
+const button = ( {children, clicked}) => {
+    return <button onClick={clicked} className="btnStyle">{children}</button>
 }
 
 export default button;
